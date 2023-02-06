ebd7215dcef208226709901f0972b87cbb6c1d6f
lucas.hsu <lucas.hsu@coretronic.com>
2023-02-06 16:05:16 +0800

    2022 / 02 / 06 添加 binarize 函式 和 輸出二值化影像 對於 binarize 函式 進行測試

--- src/image_processing/image_reader.py
+++ src/image_processing/image_reader.py
@@ -1,7 +1,10 @@
 import os
 import re
+import cv2
 import glob
+import numpy as np
 from PIL import Image
+from src.utils import logger
 
 
 def sort_by_number(filename):
@@ -17,20 +20,27 @@ def sort_by_number(filename):
     return number
 
 
-def read_images(image_dir):
+def read_and_binarize_images(image_dir):
     """讀取影像目錄內的影像，按順序更名後並存放在指定路徑
     :param image_dir: 影像目錄的路徑 (str)
     :return: images: 影像的清單 (list)
     """
     # 存儲影像的列表
     images = []
+    bin_images = []
     # 輸出路徑
     images_output = os.path.join(image_dir, 'images_output')
+    # 二值化影像的輸出路徑
+    bin_images_output = os.path.join(image_dir, 'bin_images_output')
 
     # 檢查輸出路徑是否存在，不存在則創建
     if not os.path.exists(images_output):
         print("輸出路徑不存在，已自動創建：", images_output)
         os.makedirs(images_output)
+
+    if not os.path.exists(bin_images_output):
+        print("二值化影像的輸出路徑不存在，已自動創建：", bin_images_output)
+        os.makedirs(bin_images_output)
 
     # 獲取所有影像文件名稱的列表
     image_files = glob.glob(os.path.join(image_dir, "*.png")) + glob.glob(os.path.join(image_dir, "*.jpg"))
@@ -44,15 +54,45 @@ def read_images(image_dir):
             try:
                 # 開啟影像文件
                 image = Image.open(filename)
-                # 將影像添加到列表中
-                images.append(image)
                 # 儲存讀取的影像
                 image.save(os.path.join(images_output, "%d.png" % index))
+                # PIL 轉換成 Numpy
+                image = np.asarray(image)
+                # 將影像添加到列表中
+                images.append(image)
+                # 影像二值化
+                np_bin_image = binarize(image, threshold=30)
+                # 將影像添加到列表中
+                bin_images.append(np_bin_image)
+                # Numpy 轉換成 PIL
+                bin_image = Image.fromarray(np.uint8(np_bin_image))
+                # 儲存讀取的影像
+                bin_image.save(os.path.join(bin_images_output, "%d.png" % index))
                 # 增加計數器
                 index += 1
             except Exception as e:
                 print(f"發生錯誤：{e}")
-    return images
+    return images, bin_images
+
+
+def binarize(image, threshold: int):
+    """
+    進行二值化的函數
+    :param image: numpy.ndarray, 需要進行二值化的圖片
+    :param threshold: int, 閾值
+    :return: numpy.ndarray, 二值化後的結果
+    """
+    # 檢查 threshold 是否為 int
+    if not isinstance(threshold, int):
+        raise TypeError("threshold should be an integer.")
+
+    try:
+        # 將圖片中的像素值大於閾值的設置為1，否則設置為0
+        binarized = np.where(image > threshold, 255, 0)
+        return binarized
+    except Exception as e:
+        logger.logger.error("binarization failed: " + str(e))
+        return None
 
 
 
--- tests/test_image_reader.py
+++ tests/test_image_reader.py
@@ -1,17 +1,38 @@
 import os
+import numpy as np
 import unittest
 from PIL import Image
-from src.image_processing.image_reader import read_images
+from src.image_processing.image_reader import read_and_binarize_images, binarize
 
 
 class TestImageReader(unittest.TestCase):
-    def test_read_images(self):
+    def test_load_images(self):
         # 定義影像目錄的路徑
         image_dir = "../images/test_images/"
         # 讀取影像
-        images = read_images(image_dir)
+        images = read_and_binarize_images(image_dir)
         # 檢查是否有至少一張影像被讀取
         assert len(images) > 0, f"無法讀取影像: {image_dir}"
+
+    def test_binarize(self):
+        image = np.array([[100, 150, 200], [100, 150, 200], [100, 150, 200]], dtype=np.uint8)
+        threshold = 150
+        expected = np.array([[0, 0, 255], [0, 0, 255], [0, 0, 255]], dtype=np.uint8)
+
+        result = binarize(image, threshold)
+        self.assertTrue(np.array_equal(result, expected))
+
+
+    def test_binarize_threshold_not_int(self):
+        # Arrange
+        image = np.array([[100, 200, 150], [120, 180, 210], [50, 60, 70]])
+        threshold = "150"
+
+        # Act and Assert
+        with self.assertRaises(TypeError) as context:
+            result = binarize(image, threshold)
+
+        self.assertEqual(str(context.exception), "threshold should be an integer.")
 
 
 # Run the tests if the module is run as a script
