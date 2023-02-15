cc2672da7446165d8acc3d49e32ea199dde31640
lucas.hsu <lucas.hsu@coretronic.com>
2023-02-15 10:53:52 +0800

    2022 / 02 / 15 優化 annotation_res 函式 增加詳細註解 建立 image、mask、annotation 三種json資料 增加 BATCH_SIZE 屬性來控制每份json的資料大小 使用 deepcopy 方法來避免資料因指向同一記憶體區段而互相干擾 添加各json檔案的資料欄位與值

--- src/utils/annotation.py
+++ src/utils/annotation.py
@@ -67,7 +67,22 @@ def search_sub_yaml(yaml_data: Dict[str, Any]) -> List[str]:
     # 回傳所有符合條件的檔案路徑清單
     return yaml_path
 
-def annotation_res(yaml_data):
+
+def annotation_res(yaml_data: Dict[str, Any]) -> None:
+    """
+        根據 yaml_data 中的設定，建立三個 JSON 檔案：
+            - images.json：包含所有圖片的相關訊息。
+            - mask.json：包含所有遮罩圖的相關訊息。
+            - annotation.json：包含所有標註資料的相關訊息。
+
+        Args:
+            yaml_data (Dict[str, Any]): 待處理的 YAML 資料。
+
+        Returns:
+            None
+        """
+
+    # 設定基礎資訊
     data = {
         "info": {
             "description": "CRI 2023 Hand Datasets",
@@ -76,26 +91,122 @@ def annotation_res(yaml_data):
             "organization": "Coretronic Reality Inc. AR/MR AI Technology Division"
         }
     }
-    data.setdefault("images", [])
+    # 設定手部類別資訊
+    categories = [{}, {}]
 
-    yaml_path = search_yaml(yaml_data)
+    # 建立 image、mask、annotation 資料的副本
+    data_image = data.copy()
+    data_mask = data.copy()
+    data_anno = data.copy()
+
+    # 為三個副本設定基本資訊
+    data_image.setdefault("images", [])
+    data_mask.setdefault("mask", [])
+    data_anno.setdefault("annotations", [])
+
+    # 找到所有 sub yaml 檔案
+    yaml_path = search_sub_yaml(yaml_data)
+
+    # 設定批次處理的資料數量
+    BATCH_SIZE = 5000
+
+    # 設定全域 id
     global_id = 0
-    for image_path in yaml_path:
 
+    # 確保三個資料夾存在, 若不存在則建立
+    ensure_folder(yaml_data['annotation_json'] + "/images")
+    ensure_folder(yaml_data['annotation_json'] + "/mask")
+    ensure_folder(yaml_data['annotation_json'] + "/annotation")
+
+    for image_path in yaml_path:
+        # 深度拷貝手部類別資訊
+        categories_copy = copy.deepcopy(categories)
+
+        # 讀取 yaml 檔案
         with open(image_path, "r") as stream:
             yaml_file = yaml.full_load(stream)
+
+        # 獲取圖片和遮罩路徑
         image_path = yaml_file['image_path']
-        # 獲取所有影像文件名稱的列表
-        image_files = glob.glob(os.path.join(image_path, "*.png")) + glob.glob(os.path.join(image_path, "*.jpg"))
+        mask_path = yaml_file['mask_path']
+
+        # 設定手部類別資訊
+        categories_copy[0]["HandCategoriesId"] = yaml_file["annotation"]["HandType"]
+        categories_copy[0]["GestureCategoriesId"] = yaml_file["gesture_id"]
+        categories_copy[0]["AngleCategoriesId"] = yaml_file["angle_id"]
+        categories_copy[1]["HandCategoriesId"] = yaml_file["annotation"]["ArmType"]
+
+        # 獲取所有npy文件名稱的列表
+        mask_files = glob.glob(os.path.join(mask_path, "*.npy"))
+
         # 按數字大小排序
-        image_files.sort(key=sort_by_number)
-        for index, filename in enumerate(image_files):
-            data["images"].append({"image_path": filename, "id": global_id, "fid": index, "camera": yaml_file["camera"]})
+        mask_files.sort(key=sort_by_number)
+
+        # 逐一處理檔案
+        for index, maskpath in enumerate(mask_files):
+            # 取得影像編號
+            number = int(re.findall(r'(\d+)\.(npy)', maskpath)[0][0])
+            # 取得影像檔案路徑
+            imagepath = image_path + "/{}.png".format(number)
+
+            # 填入影像資訊到字典中
+            data_image["images"].append({
+                "image_path": imagepath,  # 影像檔案路徑
+                "id": global_id,  # 影像編號
+                "fid": number,  # 影像ID
+                "camera": yaml_file["camera"]  # 相機資訊
+            })
+
+            # 填入標註資訊到字典中
+            data_mask["mask"].append({
+                "mask_path": maskpath,  # 標註檔案路徑
+                "id": global_id,  # 標註編號
+                "fid": number  # 影像ID
+            })
+
+            # 填入標註數據資訊到字典中
+            data_anno["annotations"].append({
+                "id": global_id,  # 標註編號
+                "image_id": global_id,  # 影像編號
+                "mask_id": global_id,  # 標註編號
+                "categories": yaml_file["categories"],  # 物件類別
+                "accessories": yaml_file["annotation"]["accessories"],  # 附屬品資訊
+                "user": yaml_file["User"]  # 使用者資訊
+            })
+
+            # 更新全域編號
             global_id += 1
 
-    # 將字典轉換成 JSON 格式
-    json_data = json.dumps(data)
+            # 如果已處理的資料筆數達到指定大小，就將字典轉換成 JSON 格式並寫入檔案中
+            if global_id % BATCH_SIZE == 0:
+                # 將字典轉換成 JSON 格式
+                image_anno_json = json.dumps(data_image)
+                mask_anno_json = json.dumps(data_mask)
+                anno_json = json.dumps(data_anno)
 
-    # 將 JSON 格式寫入文件
-    with open(yaml_data['annotation_json']+"/data.json", "w") as f:
-        f.write(json_data)
+                # 將 JSON 格式寫入文件
+                with open(yaml_data['annotation_json'] + "/images/images_{}.json".format(global_id), "w") as f:
+                    f.write(image_anno_json)
+                with open(yaml_data['annotation_json'] + "/mask/mask_{}.json".format(global_id), "w") as f:
+                    f.write(mask_anno_json)
+                with open(yaml_data['annotation_json'] + "/annotation/annotation_{}.json".format(global_id), "w") as f:
+                    print(yaml_data['annotation_json'] + "/annotation/annotation_{}.json".format(global_id))
+                    f.write(anno_json)
+
+                # 重新建立新的字典
+                data_image = {"images": []}
+                data_mask = {"mask": []}
+                data_anno = {"annotations": []}
+
+    # 將最後剩餘的字典輸出到 JSON 格式檔案
+    if data_image["images"] and data_mask["mask"] and data_anno["annotations"]:
+        image_anno_json = json.dumps(data_image)
+        mask_anno_json = json.dumps(data_mask)
+        anno_json = json.dumps(data_anno)
+        with open(yaml_data['annotation_json'] + "/images/images_{}.json".format(global_id), "w") as f:
+            f.write(image_anno_json)
+        with open(yaml_data['annotation_json'] + "/mask/mask_{}.json".format(global_id), "w") as f:
+            f.write(mask_anno_json)
+        with open(yaml_data['annotation_json'] + "/annotation/annotation{}.json".format(global_id), "w") as f:
+            f.write(anno_json)
+
